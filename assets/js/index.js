c868042b6533bbd30185e70a7d852d5d880e59e9
clairtonlm@gmail.com <clairtonlm@gmail.com>
2023-01-04 22:11:52 -0300

    Realizando verificação de email e senha

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,43 +1,55 @@
 //console.log(email.value.indexOf("@"));
-
-function verificarEmail(){
-    let email_data = email.value.split("@")
+function verificarEmail()
+{
+    let email_data = email.value.split("@");
     if(email_data[0].length > 4){
         if(email.value.indexOf("@") > 0){
-            if(email_data[1].indexOf(".") > 0){
-                alert("Email está completo")
-            }else{
-                alert("Está faltando ponto")
+            if(!email_data[1].indexOf(".") > 0){
+                email_alert.innerHTML = " (Está faltando o .)";
             }
-            alert("Email está válido")
         }else{
-            alert("Está faltando o @")
-        }    
-    }else{
-        alert("Email está invalido, está curto demais")
-    }
-   
-}
-verificarEmail()
-
-function verificarSenha(){
-    let senha_verd = senha.value
-    if(isNaN(parseInt(senha.value[i]))){
-        for(let i; i < senha.value.length; i++){
-            temnumero++
-            }
-        
+            email_alert.innerHTML = " (Está faltando o @)";
         }
-        
-    } 
-    if(temnumero > 0){
-        alert("Senha está ok")
-    }else{
-        alert("Tem que ter um número")
+    } else {
+        email_alert.innerHTML = " (Email inválido, curto demais)";
     }
-
-function validarFormulario(){
-    verificarEmail();
-   // verificarSenha();
-    
 }
+
+function verificarSenha()
+{
+    if(senha.value.length >= 8){
+        let temNumero = 0;
+        let soLetras = [];
+        for(let i = 0; i < senha.value.length; i++){
+            if(!isNaN(parseInt(senha.value[i]))){
+                temNumero++;
+            }else{
+                soLetras.push(senha.value[i]);
+            }
+        }
+        if(temNumero > 0){
+            let temMauisculo = 0;
+            soLetras.forEach(letra => {
+                if(letra === letra.toUpperCase()){
+                    temMauisculo++;
+                }
+            });
+            if(!temMauisculo > 0){
+                senha_alert.innerHTML = " (Tem que ter uma letra Maiuscula)";   
+            }
+        }else{
+            senha_alert.innerHTML = " (Tem que ter um numero)";
+        }
+    }else{
+        senha_alert.innerHTML = " (Senha pequena demais)";
+    }
+}
+
+function validarFormulario()
+{
+    event.preventDefault();
+    verificarEmail();
+    verificarSenha();
+}
+
+    
